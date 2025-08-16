0f1492ef6cf5247e676dae1b459b2bd4c44aeab3
sumit891 <amitkumar012.012123@gmail.com>
2025-08-16 12:21:23 +0530

    main

--- app.py
+++ app.py
@@ -15,4 +15,7 @@ def capture():
     return "✅ Login captured successfully!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
